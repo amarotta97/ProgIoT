ef9d92b85be2a0d28391141fa46c412df7ff8328
amarotta97 <37095182+amarotta97@users.noreply.github.com>
2018-08-16 22:02:29 +1000

    Added histogram for first 1k results

--- server/wserver.py
+++ server/wserver.py
@@ -1,21 +1,58 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, request
+
+from flask import Flask, render_template, send_file, make_response, request
 app = Flask(__name__)
+
 import os
+
 import sqlite3 as sql
+import sqlite3
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import io
 
 # Retrieve data from database
-def getData():
-        conn=sqlite3.connect('/home/pi/ass1/ass1.db')
-        curs=conn.cursor()
+conn=sqlite3.connect('/home/pi/ass1/ass1.db')
+curs=conn.cursor()
 
-        for row in curs.execute("SELECT * FROM statistic;"):
-                time = str(row[1])
-                temp = row[2]
-        conn.close()
-        return time, temp
+def getHistData (numSamples):
+        curs.execute("SELECT * FROM statistic ORDER BY timestamp DESC LIMIT "+str(numSamples))
+        data = curs.fetchall()
+        timestamp = []
+        temperature = []
+        for row in reversed(data):
+                timestamp.append(row[1])
+                temperature.append(row[2])
+        return timestamp, temperature
 
-# main route
+def maxRowsTable():
+        for row in curs.execute("SELECT COUNT(temperature) from statistic"):
+                maxNumberRows=row[0]
+        return maxNumberRows
+
+# Define and initialize global variables
+global numSamples
+numSamples = maxRowsTable()
+if (numSamples > 1001):
+        numSamples = 1000
+
+@app.route('/temp')
+def plot_temp():
+        timestamp, temperature  = getHistData(numSamples)
+        ys = temperature
+        fig = Figure()
+        axis = fig.add_subplot(1, 1, 1)
+        axis.set_title("Temperature [°C]")
+        axis.set_xlabel("Samples")
+        axis.grid(True)
+        xs = range(numSamples)
+        axis.plot(xs, ys)
+        canvas = FigureCanvas(fig)
+        output = io.BytesIO()
+        canvas.print_png(output)
+        response = make_response(output.getvalue())
+        response.mimetype = 'image/png'
+        return response
 
 @app.route("/")
 def home():
@@ -35,4 +72,3 @@ def list():
 if __name__ == "__main__":
         host = os.popen('hostname -I').read()
         app.run(host=host, port=80, debug=False)
-
